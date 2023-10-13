fc2ca36bf2805d1b74d65e961880848e7c2198be
hazimmarhaimi <hazimmarhaimi94@gmail.com>
2023-10-13 11:42:33 +0800

    Add project files.

---
+++ IRIST.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IRIST.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
